961c6dd419bf7964b3a1e6250bcc173da6cd7d79
Okirill <hotasice41@gmail.com>
2019-07-26 16:15:10 +0200

    added superuser admin

--- bboard/views.py
+++ bboard/views.py
@@ -1,10 +1,21 @@
 from django.http import HttpResponse
-
+from django.template import loader
+from django.shortcuts import render
 from .models import Bb
 
 
+
 def index(request):
-    s = 'Список объявлений\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
+# def index(request):
+#     template = loader.get_template('bboard/index.html')
+#     bbs = Bb.objects.order_by('-published')
+#     context = {'bbs': bbs}
+#     return HttpResponse(template.render(context, request))
+
+# def index(request):
+    # s = 'Список объявлений\r\n\r\n\r\n'
+    # for bb in Bb.objects.order_by('-published'):
+    #     s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+    # return HttpResponse(s, content_type='text/plain; charset=utf-8')
